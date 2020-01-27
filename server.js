d6b99fa31ea6ee0aab93e48482b66bea4af49434
Courtney Jackson <52590162+CoderZim@users.noreply.github.com>
2020-01-26 23:57:05 -0500

    coded but not fully tested

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const actionRouter = require("./actions/actionRouter");
 const server = express();
 
 server.use(express.json());
-server.use(log);
+// server.use(log);
 server.use("/api/projects", projectRouter);
 server.use("/api/actions", actionRouter);
 
@@ -12,12 +12,12 @@ server.get("/", (req, res, next) => {
   res.send("Holla if you hear me");
 });
 
-function log(req, res, next) {
-  req.time = Date.now();
-  console.log(
-    `A ${req.method} to ${req.originalUrl} was made at ${req.reqTime}`
-  );
-  next();
-}
+// function log(req, res, next) {
+//   req.time = Date.now();
+//   console.log(
+//     `A ${req.method} to ${req.originalUrl} was made at ${req.reqTime}`
+//   );
+//   next();
+// }
 
 module.exports = server;
